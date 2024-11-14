39b16c58e96ceb2c51ef5856951857324dd4dc30
Joel Cohen <jcohenf50@gmail.com>
2024-11-14 15:08:43 +0000

    feat: test created for converting string of multiple commands into ArrayList of Instructions

--- src/test/java/InputTests.java
+++ src/test/java/InputTests.java
@@ -32,8 +32,38 @@ public class InputTests {
         assertEquals(expected1, result1);
         assertEquals(expected2, result2);
 
+    }
 
+    @Test
+    @Description("Tests String of multiple instructions is converted to an array of valid Instructions (L,R,M)")
+    public void testValidInstructionsMultiple() {
+        ReceiveInput receiver = new ReceiveInput();
+        String input1 = "LRMRMML";
+        String input2 = "MRRRLLRM";
 
+        ArrayList<Instruction> expected1 = new ArrayList<>();
+        expected1.add(Instruction.L);
+        expected1.add(Instruction.R);
+        expected1.add(Instruction.M);
+        expected1.add(Instruction.R);
+        expected1.add(Instruction.M);
+        expected1.add(Instruction.M);
+        expected1.add(Instruction.L);
 
+        ArrayList<Instruction> expected2 = new ArrayList<>();
+        expected2.add(Instruction.M);
+        expected2.add(Instruction.R);
+        expected2.add(Instruction.R);
+        expected2.add(Instruction.R);
+        expected2.add(Instruction.L);
+        expected2.add(Instruction.L);
+        expected2.add(Instruction.R);
+        expected2.add(Instruction.M);
+
+        ArrayList<Instruction> result1 = receiver.inputToInstructions(input1);
+        ArrayList<Instruction> result2 = receiver.inputToInstructions(input2);
+
+        assertEquals(expected1, result1);
+        assertEquals(expected2, result2);
     }
 }
